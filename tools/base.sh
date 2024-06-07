551176f0aefc084647d9338b8bfc9ef8ac3839ab
shujiangle <shujiangles@gmail.com>
2024-06-07 16:39:11 +0800

    change base.sh

--- tools/base.sh
+++ tools/base.sh
@@ -3,177 +3,58 @@
 # Bootstrap script to install kubernetes env.
 #
 # This script is intended to be used for install kubernetes env.
+# version: 1.0
 
-# author: shujiangle
 
-# 本机ip
+
+
+# 安装nexus定义的ip
 LOCALIP="localhost"
 
 # 镜像版本
-IMAGETAG="1.23.17"
+IMAGETAG="1.26.15"
 
-# 拉取的用户名和密码
-USER="ptx9sk7vk7ow:003a1d6132741b195f332b815e8f98c39ecbcc1a"
+# 当前路径, $(pwd) 可以更改路径
+PKGPWD=$(pwd)
 
-# 拉取的URL
-URL="https://pixiupkg-generic.pkg.coding.net"
-
-# 当前路径, $pwd 可以更改路径
-PKGPWD=$pwd
 
 # 判断LOCALIP是否修改
-printChangeIp() {
-	if [ "$LOCALIP" = "localhost" ]; then
-		echo "你的ip未修改,请修改成你部署机ip"
+function print_change_ip() {
+    if [ "$LOCALIP" = "localhost" ]; then
+		log error "base.sh 24行:     你的ip未修改,请修改12行， LOCALIP的值"
 		exit 1
 	fi
 }
 
-printHelp() {
-	printf "[WARN] 请输入你要选择的操作.\n\n"
-	echo "Available Commands:"
-	printf "  %-25s\t %s\t \n" "download" "下载离线包"
-	printf "  %-25s\t %s\t \n" "install" "安装前置服务"
-	printf "  %-25s\t %s\t \n" "push" "推送k8s镜像和rpm包"
-	printf "  %-25s\t %s\t \n" "kubezansible" "安装kubez-ansible"
+
+# 打印日志 31m 红色，32m 绿色，34蓝色
+function log() {
+    case $1 in
+        "info")
+            echo -e `date '+%Y-%m-%d %H:%M:%S'`"\t\t\t\e[32m [ INFO ]  $2\e[0m"
+            ;;
+        "error")
+            echo -e `date '+%Y-%m-%d %H:%M:%S'`"\t\t\t\e[31m [ error ]  $2\e[0m"
+            ;;
+        *)
+            echo -e `date '+%Y-%m-%d %H:%M:%S'`"\t\t\t\e[34m [ INFO ]   $2\e[0m"
+            ;;
+    esac
 }
 
-printDownloadHelp() {
-	printf "[WARN] 请输入你要下载的离线包.\n\n"
-	echo "Available Commands:"
-	printf "  %-25s\t %s\t \n" "all" "下载所有离线包"
-	printf "  %-25s\t %s\t \n" "nexus" "下载nexus"
-	printf "  %-25s\t %s\t \n" "rpm" "下载rpm离线包"
-	printf "  %-25s\t %s\t \n" "image" "下载镜像包"
-	printf "  %-25s\t %s\t \n" "kubez" "下载kubez-ansible 离线包"
-}
 
-Download() {
-	case $1 in
-	"all")
-		downloadNexus
-		downloadRpm
-		downloadImage
-		downloadKubez
-		;;
-	"nexus")
-		downloadNexus
-		;;
-	"rpm")
-		downloadRpm
-		;;
-	"image")
-		downloadImage
-		;;
-	"kubez")
-		downloadKubez
-		;;
-	*)
-		printDownloadHelp
-		;;
-	esac
-}
+# 检查nexus状态
+#function check_nexus_status() {
+#    curl localhost:58000>/dev/null  2>&1
+#    if [ $? -eq 0 ]; then
+#        log info "nexus服务已经安装"
+#        exit
+#    fi
+#
+#}
 
-downloadNexus() {
-	# 准备 nexus 离线包
-	if [ ! -f "nexus.tar.gz" ]; then
-		echo "正在下载nexus"
-		curl -fL -u $USER $URL/pixiu/k8soffline/nexus.tar.gz?version=latest -o nexus.tar.gz
-	else
-		# nexus 的md5值
-		nexus_old_md5="f7224a5b7e4cc049fe59cb9c3898da76"
-		nexus_new_md5=$(md5sum nexus.tar.gz | awk '{print $1}')
-		if [ "$nexus_new_md5" == "$nexus_old_md5" ]; then
-			echo "nexus文件已经存在无需下载"
-		else
-			echo "nexus md5值不正确，开始重新下载"
-			curl -fL -u $USER $URL/pixiu/k8soffline/nexus.tar.gz?version=latest -o nexus.tar.gz
-		fi
-	fi
-}
 
-downloadRpm() {
-	# 准备 rpm 离线包
-	if [ ! -f "k8s-v${IMAGETAG}-rpm.tar.gz" ]; then
-		echo "正在下载k8s-v${IMAGETAG}-rpm.tar.gz"
-		curl -fL -u $USER $URL/pixiu/k8soffline/k8s-v$IMAGETAG-rpm.tar.gz?version=latest -o k8s-v${IMAGETAG}-rpm.tar.gz
-	else
-		# nexus 的md5值
-		rpm_old_md5="f95fdacb5d5d7261a3a25be69763122f"
-		rpm_new_md5=$(md5sum k8s-v${IMAGETAG}-rpm.tar.gz | awk '{print $1}')
-		if [ "$rpm_new_md5" == "$rpm_old_md5" ]; then
-			echo "k8s-v${IMAGETAG}-rpm.tar.gz文件已经存在无需下载"
-		else
-			echo "k8s-v${IMAGETAG}-rpm.tar.gz md5值不正确，开始重新下载"
-			curl -fL -u $USER $URL/pixiu/k8soffline/k8s-v$IMAGETAG-rpm.tar.gz?version=latest -o k8s-v${IMAGETAG}-rpm.tar.gz
-		fi
-	fi
-}
-
-downloadImage() {
-	# 准备镜像离线包
-	if [ ! -f "k8s-centos7-v${IMAGETAG}_images.tar.gz" ]; then
-		echo "正在下载k8s-centos7-v${IMAGETAG}_images.tar.gz"
-		curl -fL -u $USER $URL/pixiu/allimagedownload/allimagedownload.tar.gz?version=latest -o k8s-centos7-v${IMAGETAG}_images.tar.gz
-	else
-		# nexus 的md5值
-		image_old_md5="f07343496ee591ca78ad25fb27af68d9"
-		image_new_md5=$(md5sum k8s-centos7-v${IMAGETAG}_images.tar.gz | awk '{print $1}')
-		if [ "$image_new_md5" == "$image_old_md5" ]; then
-			echo "k8s-centos7-v${IMAGETAG}_images.tar.gz文件已经存在无需下载"
-		else
-			echo "k8s-centos7-v${IMAGETAG}_images.tar.gz md5值不正确，开始重新下载"
-			curl -fL -u $USER $URL/pixiu/allimagedownload/allimagedownload.tar.gz?version=latest -o k8s-centos7-v${IMAGETAG}_images.tar.gz
-		fi
-	fi
-
-}
-
-downloadKubez() {
-	# 准备镜像离线包
-	if [ ! -f "kubez-ansible-offline-master.zip" ]; then
-		echo "正在下载kubez-ansible-offline-master.zip"
-		curl https://codeload.github.com/gopixiu-io/kubez-ansible-offline/zip/refs/heads/master -o kubez-ansible-offline-master.zip
-	else
-		# nexus 的md5值
-		kubez_old_md5="f8f17f550211fdcf65f8c73e4add170a"
-		kubez_new_md5=$(md5sum kubez-ansible-offline-master.zip | awk '{print $1}')
-		if [ "$kubez_new_md5" == "$kubez_old_md5" ]; then
-			echo "kubez-ansible-offline-master.zip文件已经存在无需下载"
-		else
-			echo "kubez-ansible-offline-master.zip md5值不正确，开始重新下载"
-			curl https://codeload.github.com/gopixiu-io/kubez-ansible-offline/zip/refs/heads/master -o kubez-ansible-offline-master.zip
-		fi
-	fi
-	# 准备 kubez-ansible 离线包
-
-}
-
-check_nexus_status() {
-	curl localhost:58000 >/dev/null 2>&1
-	if [ $? == 0 ]; then
-		echo -e "服务已经启动成功"
-		sleep 1
-		exit 0
-	fi
-}
-# 安装nexus
-installNexus() {
-	check_nexus_status
-	check_memory
-	if [ ! -f "nexus.tar.gz" ]; then
-		echo "nexus安装包不存在，请下载"
-		exit 1
-	fi
-	cd $PKGPWD
-	tar zxvf nexus.tar.gz
-	cd nexus_local/
-
-	# 启动服务
-	sh nexus.sh start
-}
-
-check_memory() {
+function check_memory() {
     mem_total_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
     mem_total_gb=$(echo "scale=2; $mem_total_kb / 1024 / 1024" | bc)
     required_mem_gb=4.0
@@ -187,7 +68,54 @@ check_memory() {
 }
 
 
-printPushHelp() {
+
+# 安装nexus
+function install_nexus() {
+	check_memory
+	curl localhost:58000>/dev/null  2>&1
+    if [ $? -eq 0 ]; then
+        log info "nexus服务已经安装"
+        return 0
+    fi
+    if [ ! -d "/data/nexus_local" ]; then
+        log info "准备开始安装nexus---------"
+        [ ! -f "./nexus.tar.gz" ] &&  log error  "base.sh 78行:     nexus.tar.gz文件不存在,请下载" && exit 1
+        mkdir /data
+        tar -zxvf nexus.tar.gz -C /data
+    fi
+
+	# 启动服务
+	cd  /data/nexus_local && bash nexus.sh start
+
+    # 判断/etc/rc.d/rc.local是否存在nexus
+    line=`cat /etc/rc.d/rc.local|grep "bash nexus.sh start"|wc -l`
+    if [ "$line" -eq 0 ]; then
+        # 写入开机自启动
+       echo 'cd /data/nexus_local &&  bash nexus.sh start' >> /etc/rc.d/rc.local
+    fi
+
+}
+
+function push_nexus() {
+	case $1 in
+	"all")
+	    push_nexus_image
+		sleep 10
+		push_nexus_rpm
+		;;
+	"image")
+		push_nexus_image
+		;;
+	"rpm")
+		push_nexus_rpm
+		;;
+	*)
+		push_nexus_help
+		;;
+	esac
+}
+
+function push_nexus_help() {
 	printf "[WARN] 请输入你要上传的物料.\n\n"
 	echo "Available Commands:"
 	printf "  %-25s\t %s\t \n" "all" "上传所有k8s镜像和所有rpm包"
@@ -195,75 +123,85 @@ printPushHelp() {
 	printf "  %-25s\t %s\t \n" "rpm" "上传所有rpm包"
 }
 
-pushImage() {
-	printChangeIp
-	cd $PKGPWD
-	tar zxvf k8s-centos7-v${IMAGETAG}_images.tar.gz
+function push_nexus_image() {
+    cd $PKGPWD
+    print_change_ip
+    install_nexus
+    cd $PKGPWD
+    if [ ! -d "allimagedownload" ]; then
+        [ ! -f "./k8s-centos7-v${IMAGETAG}_images.tar.gz" ] && log error  "base.sh 128行： k8s-centos7-v${IMAGETAG}_images.tar.gz文件不存在,请检查相关文件是否存在" && exit 1
+        log info "开始解压k8s-centos7-v${IMAGETAG}_images.tar.gz"
+        tar -zxvf k8s-centos7-v${IMAGETAG}_images.tar.gz
+    fi
+
+
 	cd allimagedownload
 	sh load_image.sh $LOCALIP
 }
 
-pushRpm() {
-	printChangeIp
+function push_nexus_rpm() {
+    cd $PKGPWD
+	print_change_ip
+	install_nexus
 	cd $PKGPWD
-	tar zxvf k8s-v${IMAGETAG}-rpm.tar.gz
+	if [ ! -d "localrepo" ]; then
+    [ ! -f "./k8s-centos7-v${IMAGETAG}-rpm.tar.gz" ] &&  log error  "base.sh 144行:     k8s-centos7-v${IMAGETAG}-rpm.tar.gz文件不存在,请检查相关文件是否存在" && exit 1
+    log info "开始解压k8s-centos7-v${IMAGETAG}-rpm.tar.gz"
+    tar -zxvf k8s-centos7-v${IMAGETAG}-rpm.tar.gz
+    fi
+	cd $PKGPWD
 	cd localrepo
+	log info
 	sh push_rpm.sh $LOCALIP
 }
 
-Push() {
-	case $1 in
-	"all")
-		pushImage
-		sleep 10
-		pushRpm
-		;;
-	"image")
-		pushImage
-		;;
-	"rpm")
-		pushRpm
-		;;
-	*)
-		printPushHelp
-		;;
-	esac
-}
 
-printKubezHelp() {
+function print_kubez_ansible() {
 	printf "[WARN] 设置nexus repo和安装kubez-ansible.\n\n"
 	echo "Available Commands:"
-	printf "  %-25s\t %s\t \n" "all" "设置nexus repo和安装kubez-ansible"
-	printf "  %-25s\t %s\t \n" "repo" "设置nexus repo"
-	printf "  %-25s\t %s\t \n" "install" "安装kubez-ansible"
+	printf "     %-25s\t %s\t \n" "all" "设置nexus repo和安装kubez-ansible"
+	printf "     %-25s\t %s\t \n" "repo" "设置nexus repo"
+	printf "     %-25s\t %s\t \n" "install" "安装kubez-ansible"
 }
 
-kubezansible() {
+function kubez_ansible() {
 	case $1 in
 	"all")
-		kubezansibleRepo
-		kubezansibleInstall
+		kubez_ansible_repo
+		kubez_ansible_install
+
 		;;
 	"repo")
-		kubezansibleRepo
+		kubez_ansible_repo
 		;;
 	"install")
-		kubezansibleInstall
+		kubez_ansible_install
 		;;
 	*)
-		printKubezHelp
+		print_kubez_ansible
 		;;
 	esac
 }
 
-kubezansibleRepo() {
-	printChangeIp
+function kubez_ansible_repo() {
+    # 判断ip是否修改
+	print_change_ip
 
-	[ -d "/etc/yum.repos.d.bak" ] && echo "/etc/yum.repos.d.bak  备份目录存在，无需备份" && exit 0
+
+
+    # 判断nexus repo是否存在
+	[ -f "/etc/yum.repos.d/offline.repo" ] && log info "/etc/yum.repos.d/offline.repo 文件存在，无需再创建" && return 0
+
+    # 判断nexus服务是否启动
+    install_nexus
+
+    # 判断 /etc/yum.repos.d.bak 是否存在
+	[ -d "/etc/yum.repos.d.bak" ] && log info "/etc/yum.repos.d.bak  备份目录存在，无需备份" && return 0
+	log info
 	mv /etc/yum.repos.d /etc/yum.repos.d.bak
 	mkdir -p /etc/yum.repos.d
 
-	[ -f "/etc/yum.repos.d/offline.repo" ] && echo "/etc/yum.repos.d/offline.repo 文件存在，无需再创建" && exit 0
+
 	cat >/etc/yum.repos.d/offline.repo <<EOF
 [basenexus]
 name=Pixiuio Repository
@@ -272,53 +210,105 @@ enabled=1
 gpgcheck=0
 EOF
 
-	yum clean all && rm -rf /var/cache/yum/* && yum makecache
+	log info "\n" && yum clean all && rm -rf /var/cache/yum/* && yum makecache && echo -e "\n" && log info "repo 仓库修改完成"
 }
 
-kubezansibleInstall() {
+function kubez_ansible_install() {
 	# 判断ip是否修改
-	printChangeIp
+	print_change_ip
 
-	if command -v "kubez-ansible" >/dev/null; then
-		echo "kubez-ansible 命令已经安装"
-		exit 0
+    # 判断nexus服务是否启动
+    install_nexus
+
+
+    if command -v "kubez-ansible" >/dev/null; then
+		log true "kubez-ansible 命令安装完成"
+		exit 1
 	else
 		cd $PKGPWD
 		# 安装依赖包
+		log info
+		if [ ! -d "kubez-ansible-offline-master" ]; then
+		    [ ! -f "./kubez-ansible-offline-master.zip" ] &&  log error  "base.sh 228:     kubez-ansible-offline-master.zip文件不存在,请下载" && exit 1
+
+		fi
+        unzip kubez-ansible-offline-master.zip
 		yum makecache
 		yum -y install ansible unzip python2-pip
 		# 解压 kubez-ansible 包
-		if [ ! -d "kubez-ansible-offline-master" ]; then
-			unzip kubez-ansible-offline-master.zip
-			cd kubez-ansible-offline-master
-			# 安装依赖
-			pip install pip/pbr-5.11.1-py2.py3-none-any.whl
 
-			cp tools/git /usr/local/bin && git init
-			# 执行安装
-			python setup.py install
+        log info
+        cd kubez-ansible-offline-master
+        # 安装依赖
+        pip install pip/pbr-5.11.1-py2.py3-none-any.whl
 
-			cp -r etc/kubez/ /etc/
-			cp ansible/inventory/multinode ~
-			cd ~
-		fi
+        cp tools/git /usr/local/bin && chmod 755 /usr/local/bin/git && git init
+        # 执行安装
+        log info
+        python setup.py install
+
+        cp -r etc/kubez/ /etc/
+        cp ansible/inventory/multinode ~
+        cd ~
+
+        log info
+        if command -v "kubez-ansible" >/dev/null; then
+
+        log true "kubez-ansible 命令安装完成"
+        kubez-ansible -h
+	    fi
 	fi
+
+
 }
 
-case $1 in
-"download")
-	Download $2
-	;;
-"install")
-	installNexus
-	;;
-"push")
-	Push $2
-	;;
-"kubezansible")
-	kubezansible $2
-	;;
-*)
-	printHelp
-	;;
-esac
+
+
+
+
+
+# 初始化的帮助服务
+function printhelp() {
+    printf "[WARN] 请输入你要选择的操作.\n\n"
+    echo "Available Commands:"
+
+    # 定义命令和描述的数组
+    commands=("install" "push" "kubezansible")
+    descriptions=("安装前置服务" "推送k8s镜像和rpm包" "安装kubez-ansible")
+
+    # 遍历数组并打印每个命令和描述
+    for i in "${!commands[@]}"; {
+        printf "     %-25s\t %s\t \n" "${commands[$i]}" "${descriptions[$i]}"
+    }
+}
+
+
+function all_install() {
+    install_nexus
+    push_nexus all
+    kubez_ansible all
+}
+
+
+function input() {
+   case $1 in
+    "all")
+        all_install
+        ;;
+    "install")
+        install_nexus
+        ;;
+    "push")
+        push_nexus $2
+        ;;
+    "kubezansible")
+        kubez_ansible $2
+        ;;
+    *)
+        printhelp
+        ;;
+    esac
+
+}
+
+input  $1  $2
